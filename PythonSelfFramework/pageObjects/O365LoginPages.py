b7636c0163f75ef5ac9ce01d331a9d9deeac4bf5
bguillenschool <bguillenschool@gmail.com>
2022-02-10 16:43:07 -0800

    completed objects for O365 Login Pages

--- PythonSelfFramework/pageObjects/O365LoginPages.py
+++ PythonSelfFramework/pageObjects/O365LoginPages.py
@@ -1,23 +1,20 @@
 from selenium.webdriver.common.by import By
 
-from pageObjects.O365PwPage import O365PwPage
-
-
 class O365LoginPages:
 
     def __init__(self, driver):
         self.driver = driver
 
     emailField = (By.CSS_SELECTOR, "input[name='loginfmt']")
-    NextButton = (By.CSS_SELECTOR, "input[type='submit']")
+    nextButton = (By.CSS_SELECTOR, "input[type='submit']")
     passwordField = (By.CSS_SELECTOR, "input[type='password']")
 
-    def email_field(self):
-        return self.driver.find_element(*O365LoginPages.email_Field)
+    def email_field(self, email):
+        self.driver.find_element(*O365LoginPages.emailField).send_keys(email)
 
     def next_button(self):
-        return self.driver.find_element(*O365LoginPages.next_Button)
+        return self.driver.find_element(*O365LoginPages.nextButton)
 
     def password_field(self):
-        return self.driver.find_element(*O365LoginPages.passwordField).send_keys("Opti%6&8$")
+        self.driver.find_element(*O365LoginPages.passwordField).send_keys("Opti%6&8$")
 
